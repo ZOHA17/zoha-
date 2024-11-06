83f440142504f0e5bf09e062782ac8dcce973d2b
ZOHA17 <zohatahir983@gmail.com>
2024-11-06 10:00:27 +0500

    Add files via upload

---
+++ pattern 1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  	int rows;
+  	cout<<"Enter the number of rows you want for the pattern"<<endl;
+  	cin>>rows;
+  	for (int i=rows ; i>=1; i--)
+  	{
+  		for (int j=1; j<=i; j++)
+  		{
+  			cout<<"*";
+		  }
+		  cout<<endl;
+	  }
+	  	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
